15072b4170722c2d49f033de6efa08f1da398711
03DAICHI18 <dai.03.chi.18@gmail.com>
2020-09-16 18:51:40 +0900

    header実装

---
+++ js/script.js
@@ -0,0 +1,66 @@
+// SP用デフォルトhover制御
+addEventListener('DOMContentLoaded', function () {
+  if (isTouchDevice()) {
+    for (var i in document.styleSheets) if (document.styleSheets.hasOwnProperty(i)) {
+      deleteRuleHover(document.styleSheets[i]);
+    }
+  }
+  function isTouchDevice() {
+    return 'ontouchstart' in document.documentElement || navigator.maxTouchPoints || navigator.msMaxTouchPoints;
+  }
+  function deleteRuleHover(styleSheet) {
+    try {
+      var rules = styleSheet.cssRules || styleSheet.rules;
+      if (rules) {
+        for (var i = rules.length; i--;) {
+          var text = rules[i].selectorText;
+          if (/:hover/.test(text)) {
+            console.log(text);
+            styleSheet.deleteRule(i);
+          }
+        }
+        for (var i = rules.length; i--;) deleteRuleHover(rules[i]);
+      }
+    }
+    catch (ex) {
+      console.log(ex);
+    }
+  }
+});
+
+jQuery(document).ready(function ($) {
+  // hover制御
+  var userAgent = navigator.userAgent;
+  var more__btn = $('.m-more__btn');
+  if (userAgent.indexOf("iPhone") >= 0 || userAgent.indexOf("iPad") >= 0 || userAgent.indexOf("Android") >= 0) {
+    more__btn.on("touchstart", function () {
+      $(this).addClass("more__btnSP");
+    });
+    more__btn.on("touchend", function () {
+      $(this).removeClass("more__btnSP");
+    });
+  }
+
+  // ページ内スムーススクロール
+  $('a[href^="#"]').click(function () {
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    var speed = 500;
+    $("html, body").animate({
+      scrollTop: position
+    }, speed, "swing");
+    return false;
+  });
+
+  // ハンバーガーメニュー
+  $('.js-hamburgerbtn').on('click', function () {
+    $('.hamburgerbtn-line').toggleClass('open');
+    $('.hamburger-wrap').fadeToggle(300);
+  })
+
+  $('.hamburger__list').on('click', function () {
+    $('.hamburgerbtn-line').toggleClass('open');
+    $('.hamburger-wrap').fadeOut(300);
+  })
+});
