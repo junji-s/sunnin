39c662aadb4a4739e6abbfd05a7916cec789afc3
03DAICHI18 <dai.03.chi.18@gmail.com>
2020-09-17 12:21:25 +0900

    messageまで

--- js/top/top.js
+++ js/top/top.js
@@ -1,16 +1,16 @@
 jQuery(document).ready(function ($) {
   $(window).on('load resize', function () {
-    $('.gnav-inner').css('display', 'none');
+    $('.current-inner').css('display', 'none');
   });
 
   // 表示位置がトップからどれくらいに位置しているか
   $(window).scroll(function () {
     if ($(this).scrollTop() < 300) {
       // 200px以下だとフェードアウト
-      $('.gnav-inner').fadeOut();
+      $('.current-inner').fadeOut();
     } else {
       // 200px以上離れたらフェードイン
-      $('.gnav-inner').fadeIn();
+      $('.current-inner').fadeIn();
     }
   });
 })
