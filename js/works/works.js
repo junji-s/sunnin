27626667230c6e4f5e987eb000a7ee3ac5a8e21c
03DAICHI18 <dai.03.chi.18@gmail.com>
2020-09-21 03:49:43 +0900

    worksページ作成

---
+++ js/works/works.js
@@ -0,0 +1,18 @@
+jQuery(document).ready(function ($) {
+  // タブメニュー 
+  $(".works__tab-container:not(:first-of-type)").css("display", "none");
+  $('.tab').click(function () {
+    var index = $('.tab').index(this);
+    $('.tab').removeClass('tab-current');
+    $(this).addClass('tab-current');
+    // コンテンツの制御
+    $('.works__tab-container').hide().eq(index).fadeIn(600);
+  });
+
+  // ページネーション 
+  $('.page-num').click(function () {
+    var index = $('.page-num').index(this);
+    $('.page-num').removeClass('displaying');
+    $(this).addClass('displaying');
+  });
+});
