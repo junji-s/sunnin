e12a0da314ca703c76b0364904eb0e106cc84f57
03DAICHI18 <dai.03.chi.18@gmail.com>
2020-09-24 13:56:22 +0900

    selectタグのデフォルトスタイル消去、プルダウンアイコンはFontAwosomeから

--- js/works/works.js
+++ js/works/works.js
@@ -1,13 +1,13 @@
 jQuery(document).ready(function ($) {
   // タブメニュー 
-  $(".works__tab-container:not(:first-of-type)").css("display", "none");
-  $('.tab').click(function () {
-    var index = $('.tab').index(this);
-    $('.tab').removeClass('tab-current');
-    $(this).addClass('tab-current');
-    // コンテンツの制御
-    $('.works__tab-container').hide().eq(index).fadeIn(600);
-  });
+  // $(".works__tab-container:not(:first-of-type)").css("display", "none");
+  // $('.tab').click(function () {
+  //   var index = $('.tab').index(this);
+  //   $('.tab').removeClass('tab-current');
+  //   $(this).addClass('tab-current');
+  //   // コンテンツの制御
+  //   $('.works__tab-container').hide().eq(index).fadeIn(600);
+  // });
 
   // ページネーション 
   $('.page-num').click(function () {
